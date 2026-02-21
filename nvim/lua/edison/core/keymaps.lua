68b19af5dc2b9614854cc8706ac3b0d4033d835b
petrubear <edison.martinez@gmail.com>
2026-02-20 23:29:10 -0500

    fix: java color issues, dap plugin

--- nvim/lua/edison/core/keymaps.lua
+++ nvim/lua/edison/core/keymaps.lua
@@ -21,3 +21,80 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- LSP
+keymap.set("n", "<leader>gg", "<cmd>lua vim.lsp.buf.hover()<CR>")
+keymap.set("n", "<leader>gd", "<cmd>lua vim.lsp.buf.definition()<CR>")
+keymap.set("n", "<leader>gD", "<cmd>lua vim.lsp.buf.declaration()<CR>")
+keymap.set("n", "<leader>gi", "<cmd>lua vim.lsp.buf.implementation()<CR>")
+keymap.set("n", "<leader>gt", "<cmd>lua vim.lsp.buf.type_definition()<CR>")
+keymap.set("n", "<leader>gr", "<cmd>lua vim.lsp.buf.references()<CR>")
+keymap.set("n", "<leader>gs", "<cmd>lua vim.lsp.buf.signature_help()<CR>")
+keymap.set("n", "<leader>rr", "<cmd>lua vim.lsp.buf.rename()<CR>")
+keymap.set("n", "<leader>gf", "<cmd>lua vim.lsp.buf.format({async = true})<CR>")
+keymap.set("v", "<leader>gf", "<cmd>lua vim.lsp.buf.format({async = true})<CR>")
+keymap.set("n", "<leader>ga", "<cmd>lua vim.lsp.buf.code_action()<CR>")
+keymap.set("n", "<leader>gl", "<cmd>lua vim.diagnostic.open_float()<CR>")
+keymap.set("n", "<leader>gp", "<cmd>lua vim.diagnostic.goto_prev()<CR>")
+keymap.set("n", "<leader>gn", "<cmd>lua vim.diagnostic.goto_next()<CR>")
+keymap.set("n", "<leader>tr", "<cmd>lua vim.lsp.buf.document_symbol()<CR>")
+keymap.set("i", "<C-Space>", "<cmd>lua vim.lsp.buf.completion()<CR>")
+
+-- Filetype-specific keymaps (these can be done in the ftplugin directory instead if you prefer)
+keymap.set("n", "<leader>go", function()
+	if vim.bo.filetype == "java" then
+		require("jdtls").organize_imports()
+	end
+end)
+
+keymap.set("n", "<leader>gu", function()
+	if vim.bo.filetype == "java" then
+		require("jdtls").update_projects_config()
+	end
+end)
+
+keymap.set("n", "<leader>tc", function()
+	if vim.bo.filetype == "java" then
+		require("jdtls").test_class()
+	end
+end)
+
+keymap.set("n", "<leader>tm", function()
+	if vim.bo.filetype == "java" then
+		require("jdtls").test_nearest_method()
+	end
+end)
+
+-- Debugging
+keymap.set("n", "<leader>bb", "<cmd>lua require'dap'.toggle_breakpoint()<cr>")
+keymap.set("n", "<leader>bc", "<cmd>lua require'dap'.set_breakpoint(vim.fn.input('Breakpoint condition: '))<cr>")
+keymap.set("n", "<leader>bl", "<cmd>lua require'dap'.set_breakpoint(nil, nil, vim.fn.input('Log point message: '))<cr>")
+keymap.set("n", "<leader>br", "<cmd>lua require'dap'.clear_breakpoints()<cr>")
+keymap.set("n", "<leader>ba", "<cmd>Telescope dap list_breakpoints<cr>")
+keymap.set("n", "<leader>dc", "<cmd>lua require'dap'.continue()<cr>")
+keymap.set("n", "<leader>dj", "<cmd>lua require'dap'.step_over()<cr>")
+keymap.set("n", "<leader>dk", "<cmd>lua require'dap'.step_into()<cr>")
+keymap.set("n", "<leader>do", "<cmd>lua require'dap'.step_out()<cr>")
+keymap.set("n", "<leader>dd", function()
+	require("dap").disconnect()
+	require("dapui").close()
+end)
+keymap.set("n", "<leader>dt", function()
+	require("dap").terminate()
+	require("dapui").close()
+end)
+keymap.set("n", "<leader>dr", "<cmd>lua require'dap'.repl.toggle()<cr>")
+keymap.set("n", "<leader>dl", "<cmd>lua require'dap'.run_last()<cr>")
+keymap.set("n", "<leader>di", function()
+	require("dap.ui.widgets").hover()
+end)
+keymap.set("n", "<leader>d?", function()
+	local widgets = require("dap.ui.widgets")
+	widgets.centered_float(widgets.scopes)
+end)
+keymap.set("n", "<leader>df", "<cmd>Telescope dap frames<cr>")
+keymap.set("n", "<leader>dh", "<cmd>Telescope dap commands<cr>")
+keymap.set("n", "<leader>de", function()
+	require("telescope.builtin").diagnostics({ default_text = ":E:" })
+end)
+
--- nvim/lua/edison/plugins/dracula.lua
+++ nvim/lua/edison/plugins/dracula.lua
@@ -8,5 +8,12 @@ return {
 	config = function()
 		vim.g.dracula_colorterm = 0
 		vim.cmd("colorscheme dracula_pro")
+		-- LSP semantic tokens override Treesitter (priority 125 vs 100).
+		-- Dracula Pro doesn't define @lsp.type.modifier, so it inherits cyan.
+		-- Override it to pink to match the Treesitter @keyword.modifier intent.
+		vim.api.nvim_set_hl(0, "@lsp.type.modifier", { link = "DraculaPink" })
+		-- Dracula Pro doesn't define @lsp.mod.deprecated, so deprecated symbols
+		-- (methods, classes) have no visual indication. Add strikethrough.
+		vim.api.nvim_set_hl(0, "@lsp.mod.deprecated", { strikethrough = true })
 	end,
 }
