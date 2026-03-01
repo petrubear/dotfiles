22833d41a994721ca74bc655281190c94a9def37
petrubear <edison.martinez@gmail.com>
2026-02-28 21:10:44 -0500

    feat: enable vim mode

--- fish/aliases.fish
+++ fish/aliases.fish
@@ -1,12 +1,16 @@
-alias vi='nvim'
-alias vim='nvim'
-alias nano='nvim'
-alias list="eza -bghl --git --icons --color=automatic"
-alias ls='eza --icons'
-alias fp='ps aux | grep'
+
 alias ant='ant -logger org.apache.tools.ant.listener.AnsiColorLogger'
 alias ct='pygmentize -O style=borland'
-alias kubectl='kubecolor'
-alias tail='tspin'
+alias editor='nvim'
+alias fp='ps aux | grep'
+alias gP='git push'
+alias gf='git flush'
+alias gp='git pull'
+alias gs='git status'
 alias j='z'
-
+alias kubectl='kubecolor'
+alias list="eza -bghl --git --icons --color=automatic"
+alias ls='eza --icons'
+alias nano='nvim'
+alias vi='nvim'
+alias vim='nvim'
--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,5 @@
 if status is-interactive
-# Commands to run in interactive sessions can go here
+  fish_vi_key_bindings
 end
 zoxide init fish | source
 starship init fish | source
