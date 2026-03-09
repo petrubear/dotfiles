bd1c0d6ae33b0f6c82b4acc8726d2cabb2cf9f68
petrubear <edison.martinez@gmail.com>
2026-03-08 21:55:02 -0500

    fix: wezterm shift+enter

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,11 @@
 local ui = require("ui")
 local perf = require("perf")
--- local kb = require("keybinds")
+local kb = require("keybinds")
+
 local config = {}
+
 ui.apply_to_config(config)
--- kb.apply_to_config(config)
+kb.apply_to_config(config)
 perf.apply_to_config(config)
 
 return config
